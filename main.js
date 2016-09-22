95f4b86b71a3d13d52dad8933970df9e437bdccc
AntonVolt <antonvoltchok1@gmail.com>
2016-09-22 05:06:02 -0400

    javascript robotics and socket io testing

---
+++ main.js
@@ -0,0 +1,37 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+  
+  // Create a standard `piezo` instance on pin 3
+  var piezo = new five.Piezo(11);
+  
+  piezo.frequency(587, 1000); // Play note d5 for 1 second
+  // Plays a song
+  // piezo.play({
+  //   // song is composed by an array of pairs of notes and beats
+  //   // The first argument is the note (null means "no note")
+  //   // The second argument is the length of time (beat) of the note (or non-note)
+  //   song: [
+  //     ["C4", 1 / 4],
+  //     ["D4", 1 / 4],
+  //     ["F4", 1 / 4],
+  //     ["D4", 1 / 4],
+  //     ["A4", 1 / 4],
+  //     [null, 1 / 4],
+  //     ["A4", 1],
+  //     ["G4", 1],
+  //     [null, 1 / 2],
+  //     ["C4", 1 / 4],
+  //     ["D4", 1 / 4],
+  //     ["F4", 1 / 4],
+  //     ["D4", 1 / 4],
+  //     ["G4", 1 / 4],
+  //     [null, 1 / 4],
+  //     ["G4", 1],
+  //     ["F4", 1],
+  //     [null, 1 / 2]
+  //   ],
+  //   tempo: 100
+  // });
+});
